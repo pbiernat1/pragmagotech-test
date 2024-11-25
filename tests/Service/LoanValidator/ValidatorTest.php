ee133eaff23bef277d4b2a814671f258f20c1ea4
keedosn <keedosn@DESKTOP-PB.lan.dev>
2024-11-25 20:28:07 +0100

    Added phpunit composer command and errorneous testing case

---
+++ tests/Service/LoanValidator/ValidatorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PragmaGoTech\Interview\Test;
+
+use PHPUnit\Framework\TestCase;
+use PragmaGoTech\Interview\Service\LoanValidator\Validator;
+
+class ValidatorTest extends TestCase
+{
+    public function testSth()
+    {
+        $this->assertEquals(1, 0);
+    }
+}
