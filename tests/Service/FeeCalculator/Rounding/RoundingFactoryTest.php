0af26637bdbb115aede5c0235aa7e0b2282f73e3
Piotr Biernat <piotr1biernat@gmail.com>
2024-11-26 14:18:23 +0100

    Added RoundingFactory test

---
+++ tests/Service/FeeCalculator/Rounding/RoundingFactoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PragmaGoTech\Interview\Test;
+
+use PHPUnit\Framework\TestCase;
+use PragmaGoTech\Interview\Service\FeeCalculator\Rounding\RoundingFactory;
+use PragmaGoTech\Interview\Service\FeeCalculator\Rounding\RoundingInterface;
+use PragmaGoTech\Interview\Service\FeeCalculator\Rounding\RoundUpToFive;
+
+class RoundingFactoryTest extends TestCase
+{
+    public function testSuccessfullyCreatedRoundUpToFiveObject()
+    {
+        $roundingObject = RoundingFactory::create();
+
+        $this->assertEquals(RoundUpToFive::class, get_class($roundingObject));
+    }
+}
